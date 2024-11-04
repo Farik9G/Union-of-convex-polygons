8012bdb930c558a321118af1f10c563fa1d6bd64
Farik9G <92394604+Farik9G@users.noreply.github.com>
2024-11-04 18:05:47 +0300

    add my own logic of computing intersection by Sutheland Hodgman algorithm

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
 import tkinter as tk
-from shapely.geometry import Polygon
-from shapely.geometry import Point
-import matplotlib.pyplot as plt
 
 
 class PolygonApp:
@@ -14,7 +11,7 @@ class PolygonApp:
         self.canvas.pack()
         self.polygons = [[], []]  # Два списка для точек каждого полигона
         self.current_poly_index = 0  # Индекс текущего полигона
-        self.intersection_poly = None  # Пересечение полигонов
+        self.intersection_poly = []  # Пересечение полигонов
 
         # Добавление кнопок для управления
         self.btn_switch = tk.Button(root, text="Switch Polygon", command=self.switch_polygon)
@@ -48,15 +45,56 @@ class PolygonApp:
             print("Оба полигона должны содержать как минимум 3 вершины.")
             return
 
-        poly1 = Polygon(self.polygons[0])
-        poly2 = Polygon(self.polygons[1])
-        intersection = poly1.intersection(poly2)
+        # Вычисляем пересечение с использованием алгоритма Сазерленда-Ходжмана
+        self.intersection_poly = self.sutherland_hodgman(self.polygons[0], self.polygons[1])
+        self.draw_intersection()
 
-        if not intersection.is_empty:
-            self.intersection_poly = intersection
-            self.draw_intersection()
-        else:
-            print("Пересечение отсутствует.")
+    def sutherland_hodgman(self, subject_polygon, clip_polygon):
+        """Реализует алгоритм Сазерленда-Ходжмана для нахождения пересечения двух выпуклых полигонов."""
+        output_list = subject_polygon
+        for i in range(len(clip_polygon)):
+            input_list = output_list
+            output_list = []
+            if len(input_list) == 0:
+                break
+
+            # Определяем текущую грань отсечения
+            A = clip_polygon[i]
+            B = clip_polygon[(i + 1) % len(clip_polygon)]
+
+            for j in range(len(input_list)):
+                # Определяем текущую и предыдущую вершину
+                P = input_list[j]
+                Q = input_list[(j + 1) % len(input_list)]
+
+                # Проверяем положение точек относительно отрезка AB
+                if self.is_inside(A, B, Q):
+                    if not self.is_inside(A, B, P):
+                        intersection_point = self.compute_intersection(A, B, P, Q)
+                        output_list.append(intersection_point)
+                    output_list.append(Q)
+                elif self.is_inside(A, B, P):
+                    intersection_point = self.compute_intersection(A, B, P, Q)
+                    output_list.append(intersection_point)
+
+        return output_list
+
+    def is_inside(self, A, B, P):
+        """Определяет, находится ли точка P слева от отрезка AB."""
+        return (B[0] - A[0]) * (P[1] - A[1]) - (B[1] - A[1]) * (P[0] - A[0]) >= 0
+
+    def compute_intersection(self, A, B, P, Q):
+        """Вычисляет точку пересечения отрезков AB и PQ."""
+        dx1, dy1 = B[0] - A[0], B[1] - A[1]
+        dx2, dy2 = Q[0] - P[0], Q[1] - P[1]
+        denom = dx1 * dy2 - dy1 * dx2
+
+        if denom == 0:
+            return P  # Отрезки параллельны, возвращаем одну из точек
+
+        t = ((P[0] - A[0]) * dy2 - (P[1] - A[1]) * dx2) / denom
+        intersection_point = (A[0] + t * dx1, A[1] + t * dy1)
+        return intersection_point
 
     def draw_polygon(self):
         """Рисует текущий полигон и точки на холсте."""
@@ -78,9 +116,7 @@ class PolygonApp:
         """Рисует область пересечения полигонов."""
         if self.intersection_poly:
             self.canvas.delete("intersection")
-            x, y = self.intersection_poly.exterior.xy
-            points = list(zip(x, y))
-            self.canvas.create_polygon(points, outline='red', fill='red', stipple="gray50", width=2,
+            self.canvas.create_polygon(self.intersection_poly, outline='red', fill='red', stipple="gray50", width=2,
                                        tags="intersection")
 
     def clear_canvas(self):
@@ -88,7 +124,7 @@ class PolygonApp:
         self.canvas.delete("all")
         self.polygons = [[], []]
         self.current_poly_index = 0
-        self.intersection_poly = None
+        self.intersection_poly = []
 
 
 # Создаем окно приложения
